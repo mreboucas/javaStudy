da5b349065c8c8451585aecbd46ec9e5ab343e60
Marcelo Rebouças <marcelosreboucas@gmail.com>
2019-04-12 12:54:09 -0300

    Análise de estruturas de exceções

---
+++ src/mreboucas/java_estudo/exception/Aluno.java
@@ -0,0 +1,18 @@
+package mreboucas.java_estudo.exception;
+
+/**
+ * @author Marcelo Rebouças [marceloreboucas10@gmail.com] - 20 de fev de 2019 as 13:48:52 
+ */
+
+public class Aluno {
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
---
+++ src/mreboucas/java_estudo/exception/NomeAlunoNaoEcontradoException.java
@@ -0,0 +1,14 @@
+package mreboucas.java_estudo.exception;
+
+/**
+ * @author Marcelo Rebouças [marceloreboucas10@gmail.com] - 20 de fev de 2019 as 13:48:52 
+ */
+
+public class NomeAlunoNaoEcontradoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3633843045508336062L;
+
+}
